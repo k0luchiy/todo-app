165fed66bddd87b816a21092fd6657089a186aa5
k0luchiy <antoshka.osipov.04@mail.ru>
2023-12-29 15:32:06 +0500

    Static urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,6 +24,11 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("todos.urls")), 
 ]
+
+urlpatterns += static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 try:
     import debug_toolbar
